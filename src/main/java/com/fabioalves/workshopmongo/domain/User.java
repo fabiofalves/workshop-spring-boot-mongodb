9b73e647dd2b2b5660e19adf7f5c3bdcd739bf50
Fabio Alves <contato.figueiredofabio@gmail.com>
2025-08-27 13:44:56 -0300

    Entity user and REST working

---
+++ src/main/java/com/fabioalves/workshopmongo/domain/User.java
@@ -0,0 +1,19 @@
+package com.fabioalves.workshopmongo.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+public class User {
+
+    private String id;
+    private String name;
+    private String email;
+}
