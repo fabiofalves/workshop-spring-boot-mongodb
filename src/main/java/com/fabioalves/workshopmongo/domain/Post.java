b8c53e8fafc0807b0ba8b6be4a992cf71e0998d6
Fabio Alves <contato.figueiredofabio@gmail.com>
2025-09-15 15:07:27 -0300

    Creating Post entity with nested User

---
+++ src/main/java/com/fabioalves/workshopmongo/domain/Post.java
@@ -0,0 +1,28 @@
+package com.fabioalves.workshopmongo.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+
+@Document
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+public class Post {
+
+    @Id
+    private String id;
+    private Date date;
+    private String title;
+    private String body;
+
+    private User author;
+}
---
+++ src/main/java/com/fabioalves/workshopmongo/repositories/PostRepository.java
@@ -0,0 +1,9 @@
+package com.fabioalves.workshopmongo.repositories;
+
+import com.fabioalves.workshopmongo.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+}
