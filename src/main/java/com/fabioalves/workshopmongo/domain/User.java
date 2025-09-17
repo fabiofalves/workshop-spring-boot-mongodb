0f7e2f868cf2bd32d7c0f3d9ab7ef389bdac180d
Fabio Alves <contato.figueiredofabio@gmail.com>
2025-09-17 15:04:43 -0300

    Referencing user posts

--- src/main/java/com/fabioalves/workshopmongo/domain/User.java
+++ src/main/java/com/fabioalves/workshopmongo/domain/User.java
@@ -6,11 +6,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Document
 @NoArgsConstructor
-@AllArgsConstructor
 @Getter
 @Setter
 @EqualsAndHashCode(of = "id")
@@ -20,4 +23,13 @@ public class User {
     private String id;
     private String name;
     private String email;
+
+    @DBRef(lazy = true)
+    private List<Post> posts = new ArrayList<>();
+
+    public User(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
 }
