b6149549348d4035e24d7db0307b5f940587b9d7
Fabio Alves <contato.figueiredofabio@gmail.com>
2025-09-25 14:13:57 -0300

    getting a post by ID

---
+++ src/main/java/com/fabioalves/workshopmongo/services/PostService.java
@@ -0,0 +1,19 @@
+package com.fabioalves.workshopmongo.services;
+
+import com.fabioalves.workshopmongo.domain.Post;
+import com.fabioalves.workshopmongo.repositories.PostRepository;
+import com.fabioalves.workshopmongo.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repository;
+
+    public Post findById(String id){
+        return repository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+    }
+}
