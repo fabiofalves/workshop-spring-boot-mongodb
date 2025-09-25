32e4c32645878f84cddeaa777551d6bea62bbb3c
Fabio Alves <contato.figueiredofabio@gmail.com>
2025-09-25 14:45:36 -0300

    adding comments to posts

--- src/main/java/com/fabioalves/workshopmongo/domain/Post.java
+++ src/main/java/com/fabioalves/workshopmongo/domain/Post.java
@@ -1,19 +1,22 @@
 package com.fabioalves.workshopmongo.domain;
 
 import com.fabioalves.workshopmongo.dto.AuthorDTO;
+import com.fabioalves.workshopmongo.dto.CommentDTO;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Document
 @NoArgsConstructor
-@AllArgsConstructor
 @Getter
 @Setter
 @EqualsAndHashCode
@@ -26,4 +29,13 @@ public class Post {
     private String body;
 
     private AuthorDTO author;
+    private List<CommentDTO> comments = new ArrayList<>();
+
+    public Post(String id, Date date, String title, String body, AuthorDTO author) {
+        this.id = id;
+        this.date = date;
+        this.title = title;
+        this.body = body;
+        this.author = author;
+    }
 }
---
+++ src/main/java/com/fabioalves/workshopmongo/dto/CommentDTO.java
@@ -0,0 +1,19 @@
+package com.fabioalves.workshopmongo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class CommentDTO {
+
+    private String text;
+    private Date date;
+    private AuthorDTO author;
+}
