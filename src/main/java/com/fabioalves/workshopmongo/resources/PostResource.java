70a4e38aaee3c0ecddb5af87b887086f328b4e96
Fabio Alves <contato.figueiredofabio@gmail.com>
2025-09-29 14:23:58 -0300

    Implemented query with multiple criteria

--- src/main/java/com/fabioalves/workshopmongo/resources/PostResource.java
+++ src/main/java/com/fabioalves/workshopmongo/resources/PostResource.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -29,5 +30,17 @@ public class PostResource {
     public ResponseEntity<List<Post>> findByTitle (@RequestParam(value = "text", defaultValue = "") String text){
         text = URL.decodeParam(text);
         return ResponseEntity.ok().body(service.findByTitle(text));
+    }
+
+    @GetMapping(value = "/fullsearch")
+    public ResponseEntity<List<Post>> fullSearch
+            (@RequestParam(value = "text", defaultValue = "") String text,
+             @RequestParam(value = "minDate", defaultValue = "") String minDate,
+             @RequestParam(value = "maxDate", defaultValue = "") String maxDate){
+
+        text = URL.decodeParam(text);
+        Date min = URL.convertDate(minDate, new Date(0L));
+        Date max = URL.convertDate(minDate, new Date());
+        return ResponseEntity.ok().body(service.fullSearch(text, min, max));
     }
 }
